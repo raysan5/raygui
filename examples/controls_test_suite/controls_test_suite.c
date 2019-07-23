79b49afe17f54e9dc06d451b3547d0881458ec86
Leandro Gabriel <45115495+VelocityIsNotSpeed@users.noreply.github.com>
2019-07-23 20:09:07 -0300

    Update year of copyright notices and fix typo (#46)
    * Update year of copyright notices
    
    And add missing license information to image_exporter.c
    
    * Fix typo ("immedite")

--- examples/controls_test_suite/controls_test_suite.c
+++ examples/controls_test_suite/controls_test_suite.c
@@ -19,7 +19,7 @@
 *
 *   LICENSE: zlib/libpng
 *
-*   Copyright (c) 2018 raylib technologies (@raylibtech)
+*   Copyright (c) 2019 raylib technologies (@raylibtech)
 *
 **********************************************************************************************/
 
