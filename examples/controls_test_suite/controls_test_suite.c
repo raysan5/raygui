43209cf627a08adbce7ba272e43031caaf4a5e69
Ray <raysan5@gmail.com>
2021-12-21 23:26:30 +0100

    Update controls_test_suite.c

--- examples/controls_test_suite/controls_test_suite.c
+++ examples/controls_test_suite/controls_test_suite.c
@@ -37,6 +37,8 @@
 #include "raylib.h"
 
 #define RAYGUI_IMPLEMENTATION
+//#define RAYGUI_CUSTOM_ICONS     // It requires providing gui_icons.h in the same directory
+//#include "gui_icons.h"          // External icons data provided, it can be generated with rGuiIcons tool
 #include "../../src/raygui.h"
 
 #include <string.h>             // Required for: strcpy()
