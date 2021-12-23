110553535ba8541aa896568bd55fe182fc630bb6
Ray <raysan5@gmail.com>
2021-12-23 11:08:07 +0100

    Comment tweak

--- src/raygui.h
+++ src/raygui.h
@@ -3252,8 +3252,9 @@ int GuiTextInputBox(Rectangle bounds, const char *title, const char *message, co
 // https://stackoverflow.com/questions/4435450/2d-opengl-drawing-lines-that-dont-exactly-fit-pixel-raster
 Vector2 GuiGrid(Rectangle bounds, float spacing, int subdivs)
 {
+    // Grid lines alpha amount
     #if !defined(RAYGUI_GRID_ALPHA)
-        #define RAYGUI_GRID_ALPHA    0.15f           // Grid lines alpha amount
+        #define RAYGUI_GRID_ALPHA    0.15f
     #endif
 
     GuiControlState state = guiState;
