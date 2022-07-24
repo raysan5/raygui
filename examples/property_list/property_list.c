4e2a878e715c4aafa6ad7bd58d851221503c6e60
Ray <raysan5@gmail.com>
2022-07-25 00:51:43 +0200

    Update year

--- examples/property_list/property_list.c
+++ examples/property_list/property_list.c
@@ -11,7 +11,7 @@
 *
 *   LICENSE: zlib/libpng
 *
-*   Copyright (c) 2020-2021 Vlad Adrian (@Demizdor) and Ramon Santamaria (@raysan5)
+*   Copyright (c) 2020-2022 Vlad Adrian (@Demizdor) and Ramon Santamaria (@raysan5)
 *
 **********************************************************************************************/
 
