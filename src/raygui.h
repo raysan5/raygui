c16a98365fbe9358b7d873b5148319c781309bad
Ray <raysan5@gmail.com>
2022-11-07 13:19:53 +0100

    REVERTED: `GuiGrid()` cell compute, returning value was not right
    It was validated on `rGuiIcons()`, returned value was not correct

--- src/raygui.h
+++ src/raygui.h
@@ -3284,8 +3284,8 @@ Vector2 GuiGrid(Rectangle bounds, const char *text, float spacing, int subdivs)
         if (CheckCollisionPointRec(mousePoint, bounds))
         {
             // NOTE: Cell values must be rounded to int
-            currentCell.x = roundf((mousePoint.x - bounds.x)/spacing);
-            currentCell.y = roundf((mousePoint.y - bounds.y)/spacing);
+            currentCell.x = (float)((int)((mousePoint.x - bounds.x)/spacing));
+            currentCell.y = (float)((int)((mousePoint.y - bounds.y)/spacing));
         }
     }
     //--------------------------------------------------------------------
