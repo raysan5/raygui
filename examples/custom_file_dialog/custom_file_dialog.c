464676975187b81d38e99edc7436cfb5d548db99
Ray <raysan5@gmail.com>
2025-01-21 22:44:06 +0100

    Update custom_file_dialog.c

--- examples/custom_file_dialog/custom_file_dialog.c
+++ examples/custom_file_dialog/custom_file_dialog.c
@@ -21,6 +21,7 @@
 #include "../../src/raygui.h"
 
 #undef RAYGUI_IMPLEMENTATION            // Avoid including raygui implementation again
+
 #define GUI_WINDOW_FILE_DIALOG_IMPLEMENTATION
 #include "gui_window_file_dialog.h"
 
