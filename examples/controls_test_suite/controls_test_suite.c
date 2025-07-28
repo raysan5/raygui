d3187a70c57f5d43ec7828cc213c2eee47199217
wwderw <24878362+wwderw@users.noreply.github.com>
2025-07-28 16:16:36 -0500

    Update controls_test_suite.c
    Typo in include for raygui_icons.h file

--- examples/controls_test_suite/controls_test_suite.c
+++ examples/controls_test_suite/controls_test_suite.c
@@ -40,7 +40,7 @@
 
 #define RAYGUI_IMPLEMENTATION
 //#define RAYGUI_CUSTOM_ICONS     // It requires providing gui_icons.h in the same directory
-//#include "gui_icons.h"          // External icons data provided, it can be generated with rGuiIcons tool
+//#include "raygui_icons.h"          // External icons data provided, it can be generated with rGuiIcons tool
 #include "../../src/raygui.h"
 
 // raygui embedded styles
